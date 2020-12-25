5591dcfa7f64ecef2476b4df0e9e70c116a4912a
allamimouad <allamimouad@gmail.com>
2020-12-25 04:18:51 +0100

    hahiya wa7ad l petit modification hhhhh xD

---
+++ traitement_inscription.php
@@ -0,0 +1,19 @@
+<?php session_start();
+	include('dbInit.php');
+	$email = $_POST['email'];
+	if($item = $bd->query('SELECT * FROM user WHERE email=\''.$email.'\'')->fetch()){
+		$_SESSION['errIns'] = 'emailI';
+		header('location: inscription.php');
+	}
+	else {
+	$password = $_POST['password'];
+	$nom = $_POST['nom'];
+	$prenom = $_POST['prenom'];
+	$ville = $_POST['ville'];
+	$adress = $_POST['adress'];
+	$bd->query('INSERT INTO user(nom,prenom,email,password,ville,adress,genre) VALUES(\''.$nom.'\',\''.$prenom.'\',\''.$email.'\',\''.$password.'\',\''.$ville.'\',\''.$adress.'\',\'0\')');
+	$_SESSION['session']="on";
+	$_SESSION['user']=$bd->query('SELECT * FROM user WHERE id_user='.$bd->lastInsertId())->fetch();
+	header('location: index.php');
+}
+?>
